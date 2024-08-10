4872a775a0c900394c562cdf4a06c718b8e6e790
Ryan Morales <ryan.morales@west-mec.org>
2024-08-09 17:25:50 -0700

    end of week

--- calculateWeight2.js
+++ calculateWeight2.js
@@ -1,9 +1,10 @@
+const prompt = require('prompt-sync')();
 // require gravityFactors from "./gravityFactors.js"
 const gravityFactors = require('./gravityFactors.js');
 // create a function called calculateWeightOnPlanets
 
 // pass an argument of earthWeight
-function calculateWeightOnPlanets(earthWeight) {
+function calculateWeight(earthWeight) {
     // inside the function initialize an empty object
     const planetWeights = {};
     // the object is called planetWeights
@@ -11,15 +12,27 @@ function calculateWeightOnPlanets(earthWeight) {
     for (let planet in gravityFactors) {
 
         // assign a key, value pair to planetWeights for each iteration
-        planetWeights[planet] = earthWeight * gravityFactors[planet]
+        planetWeights[planet] = parseFloat((earthWeight * gravityFactors[planet]).toFixed(2));
+        // planetWeights[planet] = Math.round((earthWeight * gravityFactors[planet]) * 100) / 100;
         // the value of each key should be earthWeight times the value of the current
         // iteration of gravityFactors
         // Make sure the values are floats to two decimals
     }
     // return the planetWeights object from the function
-    return planetWeights;
+    console.log(planetWeights);
 }
-console.log(calculateWeightOnPlanets(100));
+function getUserWeight() {
+    console.log("Enter your weight in kg");
+    const userWeight = prompt(">");
+    console.log("Your weight is:", userWeight);
+    // create a method to take the user weight
+    // and log the weight on other planets
+    console.log("Your weight on other planets is:");
+    calculateWeight(userWeight);
+}
+global.getUserWeight = getUserWeight;
+// console.log("Your weight on other planets is:");
+// console.log(calculateWeight(100));
 // console log the planetWeights assume the function
 // is passed in an earthWeight of 100(kg)
 
